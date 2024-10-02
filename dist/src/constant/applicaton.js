af7bc9df73bb5b67aaaf31d9735091b746bd2813
Samad-11 <samadmalik04@gmail.com>
2024-10-02 17:35:44 +0530

    init3

--- dist/src/constant/applicaton.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiStartParams = exports.EApplicationEnvironment = void 0;
-var EApplicationEnvironment;
-(function (EApplicationEnvironment) {
-    EApplicationEnvironment["PRODUCTION"] = "production";
-    EApplicationEnvironment["DEVELOPMENT"] = "development";
-})(EApplicationEnvironment || (exports.EApplicationEnvironment = EApplicationEnvironment = {}));
-exports.apiStartParams = "/api/v1";
